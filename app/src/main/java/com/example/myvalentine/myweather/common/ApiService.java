dbe4fa4db660eb0faef9c424e6cc7b39df3cfc39
macanhui <mashell624@163.com>
2018-03-19 13:02:54 +0800

    update README

---
+++ app/src/main/java/com/example/myvalentine/myweather/common/ApiService.java
@@ -0,0 +1,8 @@
+package com.example.myvalentine.myweather.common;
+
+/**
+ * Created by myValentine on 16/6/1.
+ */
+public interface ApiService {
+
+}
---
+++ app/src/main/java/com/example/myvalentine/myweather/domain/Result.java
@@ -0,0 +1,7 @@
+package com.example.myvalentine.myweather.domain;
+
+/**
+ * Created by myValentine on 16/6/1.
+ */
+public class Result {
+}
